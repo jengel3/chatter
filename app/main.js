eeacdbbb5d8c504a6794deb5a054a94ef58e2c79
Jake <jake0oo0dev@gmail.com>
2015-05-13 22:57:13 -0500

    Work on UI, add shortcuts, remove toolbar

--- app/main.js
+++ app/main.js
@@ -1,19 +1,26 @@
 requirejs(["app", "router", "modules/servers/serverlist", "modules/servers/server", "modules/servers/serverlistview"], function(Chatter, Router, ServerList, Server, ServerListView) {
+	var gui = require('nw.gui');
 	Chatter.router = new Router();
 
 	Chatter.start();
 
-	var servers = new ServerList();
-	servers.fetch();
+	// var servers = new ServerList();
+	// servers.fetch();
 
-	var server = new Server({title: "Esper", host: "irc.esper.net", port: 6541})
-	servers.add(server);
-	server.save();
+	// var server = new Server({title: "Esper", host: "irc.esper.net", port: 6541})
+	// servers.add(server);
+	// server.save();
 
-	Chatter.servers = servers;
+	// Chatter.servers = servers;
 
-	var view = new ServerListView({collection: servers});
-	$('#channels').append('<ul></ul>').append(view.render().el);
+	// var view = new ServerListView({collection: servers});
+	// $('#channels ul').append(view.render().el);
 
 	// $('#add_server').popup();
+
+	
+	document.addEventListener('keydown', function(event){
+		if( event.keyCode == 123 ) { gui.Window.get().showDevTools(); }
+		if( event.keyCode == 116 ) { location.reload(); }
+	});
 });

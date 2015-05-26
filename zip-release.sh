30086536aec8e83fde199b73b750ce72380a485a
Jake <jake0oo0dev@gmail.com>
2015-05-26 14:22:30 -0500

    Change script location

---
+++ zip-release.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# This script zips all of the releases into separate zip files for
+# easy uploading to Github or wherever else necessary.
+
+for i in */; do cd "$i"; zip -r "../${i%/}.zip" "*"; echo "Zipped $i"; cd ../; done

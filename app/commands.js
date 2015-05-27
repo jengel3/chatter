a688635b569e51d56b83a0d5e14263a74b7f2608
Jake <jake0oo0dev@gmail.com>
2015-05-26 20:00:23 -0500

    missing file

---
+++ app/commands.js
@@ -0,0 +1,33 @@
+define(["app"], function (Chatter) {
+  "use strict";
+  var Commands = {};
+
+  Commands.list = [];
+
+  Commands.register = function(matcher, handler) {
+    Commands.list.push({
+      matcher: matcher,
+      handler: handler
+    });
+  };
+
+  Commands.handle = function(client, data) {
+    var commands = Commands.list;
+    var args = data.message.substring(1).split(" ");
+    var cmd = args[0];
+
+    for (var x = 0; x < commands.length; x++) {
+      var command = commands[x];
+      if (command.matcher === cmd) {
+        cmd = command;
+        break;
+      }
+    }
+    if (!cmd) {
+      return false;
+    }
+    return cmd.handler(client, data, args.slice(1));
+  };
+
+  return Commands;
+});
